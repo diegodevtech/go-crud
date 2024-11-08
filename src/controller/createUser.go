485c184a4683fa2981d80aa30a375e53a0b5ff06
diegodevtech <diegodevtechnology@gmail.com>
2024-11-08 11:53:45 -0400

    feat: implements loger layer

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,25 +1,46 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
 	// "github.com/diegodevtech/go-crud/src/configuration/rest_err"
+	"github.com/diegodevtech/go-crud/src/configuration/logger"
 	"github.com/diegodevtech/go-crud/src/configuration/validation"
 	"github.com/diegodevtech/go-crud/src/controller/model/request"
+	"github.com/diegodevtech/go-crud/src/controller/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	// "go.uber.org/zap/zapcore"
 )
 
-func CreateUser(c *gin.Context){
+func CreateUser(c *gin.Context) {
+	logger.Info("Initializing CreateUser Controller Method",
+		zap.String("journey","createUser"),
+	)
 	var userRequest request.UserRequest
 
 	err := c.ShouldBindJSON(&userRequest)
 
 	if err != nil {
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createUser"),
+		)
 		// rest_error := rest_err.NewBadRequestError(fmt.Sprintf("There are some invalid fields. Error: %s", err.Error()))
 		rest_error := validation.ValidateUserError(err)
 		c.JSON(rest_error.Code, rest_error)
 		return
 	}
 
-	fmt.Println(userRequest)
+	response := response.UserResponse{
+		ID: "test",
+		Email: userRequest.Email,
+		Name: userRequest.Name,
+		Age: userRequest.Age,
+	}
+
+	logger.Info("User created successfully",
+		zap.String("journey","createUser"),
+	)
+
+	c.JSON(http.StatusOK, response)
 }
