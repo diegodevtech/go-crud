98c01f9ad158d15e023ba40162f8f394eace7dd6
diegodevtech <diegodevtechnology@gmail.com>
2024-11-11 16:39:27 -0400

    feat: starts repository implementation

---
+++ src/model/repository/create_user_repository.go
@@ -0,0 +1,12 @@
+package repository
+
+import (
+	"github.com/diegodevtech/go-crud/src/configuration/rest_err"
+	"github.com/diegodevtech/go-crud/src/model"
+)
+
+
+
+func (ur *userRepository) CreateUser(userDomain model.UserDomainInterface) (model.UserDomainInterface, *rest_err.RestErr) {
+	
+}
