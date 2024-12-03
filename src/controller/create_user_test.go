89912cefba200494b8926452e5dbe07a44b507ad
diegodevtech <diegodevtechnology@gmail.com>
2024-12-03 17:06:58 -0400

    feat: finishes create user controller tests

--- src/controller/create_user_test.go
+++ src/controller/create_user_test.go
@@ -9,7 +9,9 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/diegodevtech/go-crud/src/configuration/rest_err"
 	"github.com/diegodevtech/go-crud/src/controller/model/request"
+	"github.com/diegodevtech/go-crud/src/model"
 	"github.com/diegodevtech/go-crud/src/test/mocks"
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
@@ -213,5 +215,60 @@ func TestUserController_CreateUser(t *testing.T) {
 		assert.EqualValues(t, http.StatusBadRequest, recorder.Code)
 	})
 
-	
+	t.Run("object_is_valid_but_service_returns_error", func(t *testing.T) {
+		recorder := httptest.NewRecorder()
+		context := GetTestGinContext(recorder)
+
+		userRequest := request.UserRequest{
+			Email:    "teste@teste.com",
+			Password: "Teste@@2020",
+			Name:     "Teste",
+			Age:      19,
+		}
+
+		domain := model.NewUserDomain(
+			userRequest.Email,
+			userRequest.Password,
+			userRequest.Name,
+			userRequest.Age,
+		)
+
+		b, _ := json.Marshal(userRequest)
+		stringReader := io.NopCloser(strings.NewReader(string(b)))
+
+		service.EXPECT().CreateUserService(domain).Return(nil, rest_err.NewInternalServerError("Error Test"))
+
+		MakeRequest(context, []gin.Param{}, url.Values{}, "POST", stringReader)
+		controller.CreateUser(context)
+		assert.EqualValues(t, http.StatusInternalServerError, recorder.Code)
+	})
+
+	t.Run("object_is_valid_and_service_returns_success", func(t *testing.T) {
+		recorder := httptest.NewRecorder()
+		context := GetTestGinContext(recorder)
+
+		userRequest := request.UserRequest{
+			Email:    "teste@teste.com",
+			Password: "Teste@@2020",
+			Name:     "Teste",
+			Age:      19,
+		}
+
+		domain := model.NewUserDomain(
+			userRequest.Email,
+			userRequest.Password,
+			userRequest.Name,
+			userRequest.Age,
+		)
+
+		b, _ := json.Marshal(userRequest)
+		stringReader := io.NopCloser(strings.NewReader(string(b)))
+
+		service.EXPECT().CreateUserService(domain).Return(domain, nil)
+
+		MakeRequest(context, []gin.Param{}, url.Values{}, "POST", stringReader)
+		controller.CreateUser(context)
+		assert.EqualValues(t, http.StatusOK, recorder.Code)
+	})
+
 }
